3d576f216bd9b68539b0be299ab5d9d62bccdfec
Shankhadeep-Ghoshal <shankhadeepghoshal1996@gmail.com>
2022-11-23 20:37:25 +0530

    initial commit

---
+++ src/main/kotlin/com/shankhadeepghoshal/kotlinfinaltask/KotlinfinaltaskApplication.kt
@@ -0,0 +1,11 @@
+package com.shankhadeepghoshal.kotlinfinaltask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinfinaltaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinfinaltaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/shankhadeepghoshal/kotlinfinaltask/KotlinfinaltaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shankhadeepghoshal.kotlinfinaltask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinfinaltaskApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
